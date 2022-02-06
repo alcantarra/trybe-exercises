421df695b12b84873e0aab59db819512a6a07434
Raphael Alcantara <alcantara.rra@gmail.com>
2022-02-06 18:15:21 -0300

    4.1

---
+++ fundamentos/04-javaScript-intro/1-primeiros-passos/scrypt.js
@@ -0,0 +1,49 @@
+// 1) / 2)
+
+// const a = 40;
+// const b = 20;
+
+// let soma = a + b;
+// let sub = a - b;
+// let mult = a * b;
+// let div = a / b;
+// let mod = a % b;
+
+//////////////////////////
+// if (a > b) {
+//   console.log(a);
+// }
+// else {
+//   console.log(b);
+// }
+
+// 3)
+
+// const a = 10;
+// const b = 20;
+// const c = 30;
+
+// if (a > b && a > c) {
+//   console.log(a);
+// }
+// else if (b > a && b > c) {
+//   console.log(b);
+// }
+// else {
+//   console.log(c);
+// }
+
+//4)
+
+// const a = -10;
+
+// if (a < 0) {
+//   console.log("negative");
+// }
+// else if (a == 0) {
+//   console.log("0");
+// }
+// else {
+//   console.log("positive");
+// }
+
